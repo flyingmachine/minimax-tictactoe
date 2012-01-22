178a5b4167c001a524c360b9a12dba45888bf4b5
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-01-22 13:28:22 -0500

    correctly generating new boards

--- phase1/tictactoe.lisp
+++ phase1/tictactoe.lisp
@@ -1,6 +1,7 @@
 ;; board should be an array
 (defparameter *board-size* 9)
 (defparameter *players* '((1 . X) (-1 . O)))
+(defparameter *starting-board* '(nil nil nil nil nil nil nil nil nil))
 
 (defun game-state (board player)
   (list board
@@ -10,18 +11,16 @@
 (defun moves (board player)
   (let ((next-player (* -1 player)))
     (mapcar (lambda (new-board)
-            )
-          (new-boards board next-player)))
+              (game-state new-board next-player))
+          (new-boards board next-player))))
 
 (defun new-boards (board player-making-move)
-  (let ((boards '()))
-    (labels ((f (processed current remaining)
-                (unless (null current)
-                  (if current
-                      (append (proccessed (list current) remaining))
-                    (append (board (list (append (processed (list player-making-move) remaining)))))))
-                (f (append (processed) (list current)) (car remaining) (cdr remaining))
-                ))
-      (f '() (car board) (cdr board)))))
+  (labels ((add-board (acc processed remaining)
+                      (append acc (list (append processed (list player-making-move) remaining))))
+           (f (processed current remaining acc)
+               (cond ((eq (list-length processed) *board-size*) acc)
+                     ((null current) (f (append processed (list current)) (car remaining) (cdr remaining) (add-board acc processed remaining)))
+                     (t (append processed (list current)) (car remaining) (cdr remaining) acc))))
+    (f '() (car board) (cdr board) '())))
 
 (defun draw-board (board))
