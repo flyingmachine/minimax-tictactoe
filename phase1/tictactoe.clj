8e1587f2eff8bd2779a657115a430fe68ef88c1b
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-06-03 09:37:18 -0400

    beginning to add clojure version

---
+++ phase1/tictactoe.clj
@@ -0,0 +1,34 @@
+(ns fms.tictactoe)
+(declare generate-moves new-boards)
+
+(defn game-state [player-who-just-moved board]
+  (list player-who-just-moved
+        board
+        (generate-moves player-who-just-moved board)))
+
+;; TODO ensure that this returns a vector
+(defn generate-moves [player-who-just-moved board]
+  (let [next-player (* -1 player-who-just-moved)]
+    (map (partial game-state next-player) (new-boards next-player board))))
+
+(defn new-boards [player-making-move board]
+  "given a board, generates new boards for every empty cell"
+  (letfn [(add-board [acc processed remaining]
+            (conj acc (into (conj processed player-making-move) remaining)))
+          (process-board-cell [processed current remaining acc]
+            (cond (= (count processed) (count board)) acc
+                  (nil? current) (process-board-cell (conj processed current) (first remaining) (rest remaining) (add-board acc processed remaining))
+                  true (process-board-cell (conj processed current) (first remaining) (rest remaining) acc)))]
+    (process-board-cell [] (first board) (rest board) [])))
+
+(defn x? [player]
+  (= player 1))
+
+(defn moves [game-state]
+  (last  game-state))
+
+(defn board [game-state]
+  (second game-state))
+
+(defn player [game-state]
+  (first game-state))
