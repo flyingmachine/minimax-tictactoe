4e9a7544e4b03809cef5d728002e2307394807c8
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-06-05 07:48:35 -0400

    more progress

--- phase1/tictactoe.clj
+++ phase1/tictactoe.clj
@@ -1,15 +1,17 @@
 (ns fms.tictactoe)
-(declare generate-moves new-boards)
+(declare generate-moves-memo new-boards-memo)
 
 (defn game-state [player-who-just-moved board]
   (list player-who-just-moved
         board
-        (generate-moves player-who-just-moved board)))
+        (generate-moves-memo player-who-just-moved board)))
 
 ;; TODO ensure that this returns a vector
 (defn generate-moves [player-who-just-moved board]
   (let [next-player (* -1 player-who-just-moved)]
-    (map (partial game-state next-player) (new-boards next-player board))))
+    (map (partial game-state next-player) (new-boards-memo next-player board))))
+
+(def generate-moves-memo (memoize generate-moves))
 
 (defn new-boards [player-making-move board]
   "given a board, generates new boards for every empty cell"
@@ -21,8 +23,17 @@
                   true (process-board-cell (conj processed current) (first remaining) (rest remaining) acc)))]
     (process-board-cell [] (first board) (rest board) [])))
 
+(def new-boards-memo (memoize new-boards))
+
 (defn x? [player]
   (= player 1))
+
+(defmacro with-gs-vars [[game-state & vars] & body]
+  "Example usage: (with-gs-vars (game-state moves vars) (body))"
+  (let [evald-game-state-name (gensym)]
+    `(let [~evald-game-state-name ~game-state
+           ~@(reverse (reduce into () (map (fn [var] `(~var (~var ~evald-game-state-name))) vars)))]
+       ~@body)))
 
 (defn moves [game-state]
   (last  game-state))
@@ -34,13 +45,59 @@
   (first game-state))
 
 (defn cell-for-display [cell]
-  (cond (= cell 1) "X"
-        (= cell -1) "O"
-        true " "))
+  (cond (= cell 1) " X "
+        (= cell -1) " O "
+        true "   "))
 
 (defn row [board rownum]
   (reduce (fn [result current] (conj result (cell-for-display current))) [] (subvec board (* 3 (dec rownum)) (* 3 rownum))))
 
-(defn print-row [board rownum]
-  (let [r (row board rownum)]
-    (format " %s | %s | %s " (r 0) (r 1) (r 2))))
+(defn rows [board]
+  (partition 3 board))
+
+(defn cols [board]
+  (vals (group-by #(mod % 3) board)))
+
+(defn row-display [row]
+  (str (apply str (interpose \| (map cell-for-display row))) "\n"))
+
+(defn board-display [board]
+  (apply str (interpose "-----------\n" (map row-display (rows board)))))
+
+(defn print-board [board]
+  (print (board-display board)))
+
+;; Ranking
+(defn board-full? [board]
+  (empty? (filter nil? board)))
+
+(defn win-condition-met? [board win-condition]
+  (let [p1 (board (first  win-condition))
+        p2 (board (second win-condition))
+        p3 (board (last   win-condition))]
+    (and p1 p2 p3 (= p1 p2 p3))))
+
+(defn winner [board]
+  (let [win-conditions '(;; horizontal wins
+                         (0 1 2)
+                         (3 4 5)
+                         (6 7 8)
+                         
+                         ;; vertical wins
+                         (0 3 6)
+                         (1 4 7)
+                         (2 5 8)
+                         
+                         ;; diagonal wins
+                         (0 4 8)
+                         (6 4 2))]
+    (or (when-let [win (first (drop-while (complement (partial win-condition-met? board)) win-conditions))] (board (first win)))
+        (if (board-full? board) 0))))
+
+(defn ranking [game-state]
+  (with-gs-vars [game-state moves player]
+    (let [win (winner board)]
+      (cond win win
+            moves (let [rank-fun (if (x? player) min max)]
+                    (apply rank-fun (map ranking moves)))))))
+
